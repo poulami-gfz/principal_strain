6c494fc75697741863b05988d0591d9a0b553a4b
Poulami Roy <80957700+poulami-gfz@users.noreply.github.com>
2021-10-14 18:34:48 +0200

    Add files via upload

---
+++ principal_strain.h
@@ -0,0 +1,82 @@
+/*
+  Copyright (C) 2020 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef _aspect_postprocess_visualization_principal_strain_h
+#define _aspect_postprocess_visualization_principal_strain_h
+
+#include <aspect/postprocess/visualization.h>
+#include <aspect/simulator_access.h>
+
+#include <deal.II/numerics/data_postprocessor.h>
+
+
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      /**
+       * A class derived from DataPostprocessor that outputs the principal
+       * strain values and directions, i.e., the eigenvalues and eigenvectors
+       * of the strain tensor (or optionally of the deviatoric strain tensor).
+       */
+      template <int dim>
+      class PrincipalStrain : public SimulatorAccess<dim>, public DataPostprocessor<dim>, public Interface<dim>
+      {
+        public:	
+          PrincipalStrain ();
+
+          /**
+           * Declare the parameters this class takes through input files.
+           */
+//          static
+//          void
+//          declare_parameters (ParameterHandler &prm);
+
+          /**
+           * Read the parameters this class declares from the parameter file.
+           */
+          void
+          parse_parameters (ParameterHandler &prm) override;
+          
+
+          std::vector<std::string>
+          get_names () const override;
+
+          std::vector<DataComponentInterpretation::DataComponentInterpretation>
+          get_data_component_interpretation () const override;
+
+          UpdateFlags
+          get_needed_update_flags () const override;
+
+          void
+          evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
+                                std::vector<Vector<double> > &computed_quantities) const override;
+          const std::string field_name;
+          //const auto &viz;//
+      };
+      
+    }
+  
+  }
+}
+#endif
